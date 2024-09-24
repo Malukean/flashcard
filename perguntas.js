99f383a885941a8ada91ed659dede243a51c950e
salamaleico <106350687+Malukean@users.noreply.github.com>
2024-09-24 09:14:38 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,99 @@
+criaCartao (
+    'Geografia' ,
+    'Pergunta ENEM sobre Geografia: Qual processo geológico é o principal responsável pela formação de montanhas como o Himalaia?',
+     'Deriva continental')
+    
+    criaCartao (
+    'História',
+    'Quem foi o presidente do Brasil durante a Proclamação da República?',
+    'Marechal Deodoro da Fonseca')
+    
+    criaCartao (
+    'Matemática',
+    'Qual é a fórmula da área de um círculo?',
+    'A = πr²')
+    
+    criaCartao (
+    'Química',
+    'Qual é o elemento químico de número atômico 8?',
+    'Oxigênio')
+    
+    criaCartao (
+    'Biologia',
+    'quais são as características dos fungos?',
+    'eucariontes, pluri ou unicelulares, heterotrofos,mecélio(hifas unidas)')
+    
+    criaCartao (
+    'Física',
+    'Qual é a unidade de medida da força no Sistema Internacional de Unidades?',
+    'Newton')
+    
+    criaCartao (
+    'Português',
+    'Qual é a figura de linguagem em “O vento cantava uma canção triste”?',
+    'Personificação')
+    
+    criaCartao (
+    'Literatura',
+    'elementos da literatura gótica ?',
+    'ruptuda de padrões literários,afronta racionalismo,instinto vida e morte, libido e terror')
+    
+    criaCartao (
+    'História',
+    'Qual foi o tratado que dividiu o mundo entre Portugal e Espanha em 1494?',
+    'tratado de tordesilhas')
+    
+    criaCartao (
+    'Filosofia',
+    'qual a contribuição de kant?',
+    'reune racionalismo e empiri, revolução copernicana')
+    
+    criaCartao (
+    'Geografia',
+    ' o que é agropecuária extensiva ?',
+    'áreas extensas, baixa modernidade, mão de obra familiar ')
+    
+    criaCartao (
+    'Biologia',
+    'onde ocorre digestão de proteínas e gorduras respectivamente ?',
+    'estômago e intestino ')
+    
+    criaCartao (
+    'Física',
+    'Qual é a velocidade da luz no vácuo?',
+    'Aproximadamente 300.000 km/s')
+    
+    criaCartao (
+    'Matemática',
+    '?Qual a formula da PG',
+    'An = a1 • r^(n-1)')
+    
+    criaCartao (
+    'Química',
+    'Parâmetros para classificação da cadeia carbônica?',
+    'A cadeia carbônica é classificada por ligação, forma, ramificação, composição, arranjo de átomos e número de carbonos, permitindo a identificação de moléculas orgânicas.')
+    
+    criaCartao (
+    'Geografia',
+    'Qual é a capital da Austrália?',
+    'Camberra')
+    
+    criaCartao (
+    'História',
+    'Em que ano aconteceu a Revolução Francesa?',
+    '1789')
+    
+    criaCartao (
+    'Português',
+    'Qual é a função da conjunção “mas” em uma frase?',
+    'Introduzir uma ideia de oposição')
+    
+    criaCartao (
+    'Química',
+    'Qual a fórmula molecular da glicose?',
+    'C6H12O6')
+    
+    criaCartao (
+    'Literatura',
+    'Qual é o movimento literário associado ao poeta Vinicius de Moraes?',
+    'Modernismo')
