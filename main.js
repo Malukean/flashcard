0e91171121581b9d2d8956c19f45435b74fe7c15
salamaleico <106350687+Malukean@users.noreply.github.com>
2024-10-24 10:36:40 -0300

    .

---
+++ main.js
@@ -0,0 +1,9 @@
+function mostraResposta(card){
+    const resposta = card.querySelector('.cartao__conteudo__resposta')
+if(resposta.style.display === 'none'){
+    resposta.style.display = 'block'
+}
+else{
+    resposta.style.display = 'none'
+}
+}
